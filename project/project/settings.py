8adbedac72d011d8bb0fb48f56fb888bc22a6738
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-04-30 11:46:54 -0400

    created base

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$a7q6^kqanm+av9qb5mxz4j$uo8633e%8c-oae3-up3itdzmy6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
