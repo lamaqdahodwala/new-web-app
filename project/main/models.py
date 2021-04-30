809daec9768ebd53421988a46c49c4c563473744
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-04-30 11:51:30 -0400

    created models

--- project/main/models.py
+++ project/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    upvotes = models.IntegerField(default=0)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.TextField()
+    upvotes = models.IntegerField(default=0)
